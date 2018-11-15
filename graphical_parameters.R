d8796d8cd59b0ddbb92c8dd75f01987beb8a583a
danilinares <danilinares@gmail.com>
2018-11-15 11:32:24 +0100

    1

---
+++ graphical_parameters.R
@@ -0,0 +1,25 @@
+one_column_width <- 3.42
+two_column_width <- 7.08661
+size_line <- .1
+text_labels_model <-  c( "No bias", "Sensory", "Decisional", "Sensory +\ndecisional")
+text_labels_model_asym <-  c( "No bias", "Sensory", "Response bias", "Asym crit")
+
+col_no_bias <- "grey"
+col_sensory <- "#4daf4a"
+col_decision <- "#377eb8"
+col_full <- "#e41a1c"
+col_response <- "#fb9a99"
+
+text_zero <- "No bias"
+text_sensory <- "Sensory bias"
+text_decision <- "Decisional bias"
+text_response <- "Sensory + symmetric decisional"
+text_full <- "Sensory + decisional"
+text_reference <- "Reference"
+text_best_model <- "Best model"
+text_orientation <- "Orientation (deg)"
+text_sensory_bias <- "Sensory bias (deg)"
+text_decisional_bias <- "Decisional bias (deg)"
+text_asym_crit <- "Asymmetric decisional bias"
+text_prob_sym <- expression(paste("Probability of ", italic("clockwise")))
+text_prob_asym <- expression(paste("Probability of ", italic("aligned")))
---
+++ parameters.R
@@ -0,0 +1,6 @@
+alpha <- 0.05
+
+pini_origin <- c(-1.5, 1.5)
+pini_origin_asym <- c(-2.5, 2.5)
+pini_scale <- c(0.1, 3)
+pini_lapse <- c(0, .4)
